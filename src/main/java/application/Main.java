1576763fa7f64aa7a860ae53a9ccd6d67005e703
Harald Billstein <harald.billstein@gmail.com>
2020-02-09 21:41:29 +0100

    .

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,13 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        InetAddress inetAddress = null;
+        try {
+            inetAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+        System.out.println("Host Name:- " + inetAddress.getHostName());
     }
 }
